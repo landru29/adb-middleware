f7b328e4f7cf1eec094a21f05b1f05a7ef29c29f
landru29 <cmeichel@free.fr>
2014-11-18 15:10:08 +0100

    in progress

--- src/adb-shell.c
+++ src/adb-shell.c
@@ -18,7 +18,7 @@ void ReadFromPipe(void);
 void ErrorExit(PTSTR); 
 
 
-int main(int argc, char** argv) {
+/*int main(int argc, char** argv) {
 	char inData[65536] = {0};
 	char shellAnswer[65536] = {0};
 	char command[65536] = {0};
@@ -40,7 +40,81 @@ int main(int argc, char** argv) {
 	} while (strcmp(inData, "exit") != 0);
 	pclose(inOut);
 	return 0;
-}
+}*/
+
+int main(int argc, TCHAR *argv[]) 
+{ 
+   SECURITY_ATTRIBUTES saAttr; 
+ 
+   //printf("\n->Start of parent execution.\n");
+
+// Set the bInheritHandle flag so pipe handles are inherited. 
+ 
+   saAttr.nLength = sizeof(SECURITY_ATTRIBUTES); 
+   saAttr.bInheritHandle = TRUE; 
+   saAttr.lpSecurityDescriptor = NULL; 
+
+// Create a pipe for the child process's STDOUT. 
+ 
+   if ( ! CreatePipe(&g_hChildStd_OUT_Rd, &g_hChildStd_OUT_Wr, &saAttr, 0) ) 
+      ErrorExit(TEXT("StdoutRd CreatePipe")); 
+
+// Ensure the read handle to the pipe for STDOUT is not inherited.
+
+   if ( ! SetHandleInformation(g_hChildStd_OUT_Rd, HANDLE_FLAG_INHERIT, 0) )
+      ErrorExit(TEXT("Stdout SetHandleInformation")); 
+
+// Create a pipe for the child process's STDIN. 
+ 
+   if (! CreatePipe(&g_hChildStd_IN_Rd, &g_hChildStd_IN_Wr, &saAttr, 0)) 
+      ErrorExit(TEXT("Stdin CreatePipe")); 
+
+// Ensure the write handle to the pipe for STDIN is not inherited. 
+ 
+   if ( ! SetHandleInformation(g_hChildStd_IN_Wr, HANDLE_FLAG_INHERIT, 0) )
+      ErrorExit(TEXT("Stdin SetHandleInformation")); 
+ 
+// Create the child process. 
+   
+   CreateChildProcess();
+
+// Get a handle to an input file for the parent. 
+// This example assumes a plain text file and uses string output to verify data flow. 
+ 
+   if (argc == 1) 
+      ErrorExit(TEXT("Please specify an input file.\n")); 
+
+   g_hInputFile = CreateFile(
+       argv[1], 
+       GENERIC_READ, 
+       0, 
+       NULL, 
+       OPEN_EXISTING, 
+       FILE_ATTRIBUTE_READONLY, 
+       NULL); 
+
+   if ( g_hInputFile == INVALID_HANDLE_VALUE ) 
+      ErrorExit(TEXT("CreateFile")); 
+ 
+// Write to the pipe that is the standard input for a child process. 
+// Data is written to the pipe's buffers, so it is not necessary to wait
+// until the child process is running before writing data.
+ 
+   WriteToPipe(); 
+   printf( "\n->Contents of %s written to child STDIN pipe.\n", argv[1]);
+ 
+// Read from pipe that is the standard output for child process. 
+ 
+   printf( "\n->Contents of child process STDOUT:\n\n", argv[1]);
+   ReadFromPipe(); 
+
+   printf("\n->End of parent execution.\n");
+
+// The remaining open handles are cleaned up when this process terminates. 
+// To avoid resource leaks in a larger application, close handles explicitly. 
+
+   return 0; 
+} 	
 
 void CreateChildProcess()
 // Create a child process that uses the previously created pipes for STDIN and STDOUT.
@@ -52,7 +126,7 @@ void CreateChildProcess()
    BOOL bSuccess = FALSE; 
 
    realAdbPath = getAdbPath();
-   sprintf(command, "\"\"%s\" \"shell\"\"", realAdbPath);
+   sprintf(command, "\"%s\" \"shell\"", realAdbPath);
  
 // Set up members of the PROCESS_INFORMATION structure. 
  
@@ -70,7 +144,8 @@ void CreateChildProcess()
  
 // Create the child process. 
     
-   bSuccess = CreateProcess(NULL, 
+   bSuccess = CreateProcess(
+   	  NULL, 
       (TCHAR*)command,     // command line 
       NULL,          // process security attributes 
       NULL,          // primary thread security attributes 
@@ -103,11 +178,13 @@ void WriteToPipe(void)
    DWORD dwRead, dwWritten; 
    CHAR chBuf[BUFSIZE];
    BOOL bSuccess = FALSE;
+   //HANDLE hParentStdIn = GetStdHandle(STD_INPUT_HANDLE);
  
    for (;;) 
    { 
-      bSuccess = ReadFile(g_hInputFile, chBuf, BUFSIZE, &dwRead, NULL);
-      if ( ! bSuccess || dwRead == 0 ) break; 
+   	  fscanf(stdin, "%s", &chBuf);
+      /*bSuccess = ReadFile(g_hInputFile, chBuf, BUFSIZE, &dwRead, NULL);
+      if ( ! bSuccess || dwRead == 0 ) break;*/ 
       
       bSuccess = WriteFile(g_hChildStd_IN_Wr, chBuf, dwRead, &dwWritten, NULL);
       if ( ! bSuccess ) break; 
